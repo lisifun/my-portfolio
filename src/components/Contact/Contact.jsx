77311d6f962e5639988ee1a1a8439faa30cd4522
lisifun <lisandrafj99@gmail.com>
2024-02-03 14:57:23 -0500

    adding contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,28 @@
 import "./Contact.css";
+import logo from "../../assets/images/IMG_6725.png";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="contact" id="contact">
+      <div className="about-left">
+        <div className="logo-name">
+          <img className="logo" src={logo} />
+          <div className="text">lisifun</div>
+        </div>
+        <div className="about-accounts">
+          <a
+            className="linkedin-icon"
+            href="https://www.linkedin.com/in/lisandrafundora/"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a className="github-icon" href="https://github.com/lisifun">
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import About from "../../components/About/About";
+import Contact from "../../components/Contact/Contact";
 import Presentation from "../../components/Presentation/Presentation";
 import Projects from "../../components/Projects/Projects";
 import Skills from "../../components/Skills/Skills";
@@ -10,6 +11,7 @@ const MainPage = () => {
       <About />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 };
